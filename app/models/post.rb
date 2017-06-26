d5ee415802337d4442c1f9f51ffe3e3b20f6976d
David Renz <davidrenz60@gmail.com>
2017-06-26 10:42:24 -0700

    practice changing voteable gem version: update module name

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
-  include Voteable
   include Sluggable
+  include Voteable
 
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   has_many :comments
